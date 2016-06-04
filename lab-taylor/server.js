9df7bf6b9c2211f839c0fbfa5a556ecdf59602ec
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-06-04 14:57:30 -0700

    hook up player router to server

--- lab-taylor/server.js
+++ lab-taylor/server.js
@@ -3,14 +3,20 @@
 const express = require('express');
 const debug = require('debug')('soccer:server');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 const AppError = require('./lib/AppError');
 const errorResponse = require('./lib/error-response');
+const playerRouter = require('./route/player-router');
 const port = process.env.PORT || 3000;
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost/soccerapp';
 const app = express();
 
+mongoose.connect(mongoURI);
 
 app.use(logger('dev'));
 app.use(errorResponse);
+
+app.use('/api', playerRouter);
 
 app.all('*', ((req, res) => {
   debug('* 404');
